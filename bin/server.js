1f1dfcd4ead244b4bc97a819d97153f7a74241c2
Stephen Mathieson <smath23@gmail.com>
2013-08-29 10:05:36 -0400

    add config file support
    closes #6

--- bin/server.js
+++ bin/server.js
@@ -3,6 +3,7 @@
 var bc = require('..'),
     express = require('express'),
     path = require('path'),
+    fs = require('fs'),
     Command = require('commander').Command;
 
 var program = new Command('bitbucket-component');
@@ -13,17 +14,33 @@ program
   .option('-w, --password [password]', 'bitbucket.org password')
   .option('-d, --directory [directory]', 'directory for repositories')
   .option('-p, --port [port]', 'port for server', 3000)
+  .option('-c, --config [file]', 'config file')
+  .option('--verbose', 'verbose output')
   .parse(process.argv);
 
-if (!program.directory) program.directory = path.join(process.cwd(), 'repos');
-
-var app = express();
-
-app.use(express.logger('dev'));
-app.use(bc({
+var config = {
+  port: program.port,
   username: program.username,
   password: program.password,
-  directory: program.directory
-}));
+  directory: program.directory || path.join(process.cwd(), 'repos'),
+  verbose: program.verbose
+};
 
-app.listen(program.port);
+if (program.config) {
+  var json = JSON.parse(fs.readFileSync(program.config));
+  for (var opt in json) {
+    config[opt] = json[opt];
+  }
+}
+
+var app = module.exports = express();
+
+app.use(bc(config));
+
+if (config.verbose) app.use(express.logger('dev'));
+
+app.listen(config.port, function () {
+  if (config.verbose) {
+    console.log('component server listening on %d', config.port);
+  }
+});
